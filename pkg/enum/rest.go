bb49ef074dfda2a661d8b65fd2b0cd97ddff0e6c
vladislav <ve@incsub.com>
2021-09-18 10:05:41 +0200

    Fix rest username detection on multisite

--- pkg/enum/rest.go
+++ pkg/enum/rest.go
@@ -9,6 +9,7 @@ import (
 )
 
 func enumerateAuthorId(url string) func(wp_http.Client, ...int) (map[string]int, error) {
+	url = wp_http.NormalizeRootUrl(url)
 	return func(client wp_http.Client, limit ...int) (map[string]int, error) {
 		maxUsers := 100
 		if len(limit) > 0 {
@@ -17,14 +18,21 @@ func enumerateAuthorId(url string) func(wp_http.Client, ...int) (map[string]int,
 		result := make(map[string]int)
 		var overallErr error
 		for i := 1; i < maxUsers; i++ {
-			author_url := fmt.Sprintf("%s?author=%d", wp_http.NormalizeRootUrl(url), i)
+			author_url := fmt.Sprintf("%s?author=%d", url, i)
 			resp := client.Send(author_url)
 			if resp.StatusCode < 100 {
 				overallErr = errors.New("passthrough")
 			} else if resp.StatusCode > 300 && resp.StatusCode < 400 {
 				location := resp.Header.Get("location")
-				rpl := fmt.Sprintf("%sauthor/", url)
-				user := strings.Replace(strings.Replace(location, rpl, "", 1), "/", "", -1)
+				authorParam := strings.LastIndex(location, "/author/")
+				var rawUser string
+				if authorParam >= 0 {
+					rawUser = location[authorParam+8:]
+				} else {
+					rpl := fmt.Sprintf("%sauthor/", url)
+					rawUser = strings.Replace(location, rpl, "", 1)
+				}
+				user := strings.Replace(rawUser, "/", "", -1)
 				result[user] = i
 			}
 		}
