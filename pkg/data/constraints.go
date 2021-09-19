e725eb88e2bcd3122cc27b133bcb9e4806f567ff
vladislav <ve@incsub.com>
2021-09-19 07:28:26 +0200

    Add UA randomization to constraints

--- pkg/data/constraints.go
+++ pkg/data/constraints.go
@@ -1,17 +1,19 @@
 package data
 
 type Constraints struct {
-	URL   string
-	Kind  int
-	Start int
-	End   int
+	URL      string
+	Kind     int
+	Start    int
+	End      int
+	RandomUA bool
 }
 
 func DefaultConstraints() Constraints {
 	return Constraints{
-		URL:   "",
-		Kind:  0,
-		Start: 1,
-		End:   10,
+		URL:      "",
+		Kind:     0,
+		Start:    1,
+		End:      10,
+		RandomUA: false,
 	}
 }
